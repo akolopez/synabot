08fa72f55072ef7af0f1957121da079576ffb9dc
Miguel Lopez <miguel.lopez@synacy.com>
2015-09-23 00:35:28 +0800

    Remove unused commands.
    Signed-off-by: Miguel Lopez <miguel.lopez@synacy.com>

--- lib/synabot/command/parser.rb
+++ lib/synabot/command/parser.rb
@@ -6,22 +6,10 @@ module Synabot
 		class Parser
 
 			def self.parse(message)
-				command, arg1, arg2 = message.split
-				if command == 'summon'
-					Synabot::Command::Type::Summon.do(arg1)
-				elsif command == '!points' || command == '!pt'
-					Synabot::Command::Type::Points.do(arg1)
-				elsif command == '!commend' || command == '!cm'
-					if arg2
-						giver = message.from.node.split('_')[1]
-						Synabot::Command::Type::Commend.do(arg1, giver, message.body.split()[2..-1].join(' '))
-					else 
-						Synabot::Command::Type::Commend.do(arg1)
-					end
-				elsif command == '!register' || command == '!reg'
-					Synabot::Command::Type::Register.do(arg1, arg2)
-				elsif command == 'gif'
-					Synabot::Command::Type::Giphy.do(arg1)
+				command = message.split.first
+				args = message[command.length..-1]
+				if command == 'gif'
+					Synabot::Command::Type::Giphy.do(args)
 				elsif command == 'claire'
 					Synabot::Command::Type::Claire.do()
 				end
--- lib/synabot/command/type/commands.rb
+++ lib/synabot/command/type/commands.rb
@@ -1,6 +1,2 @@
-require_relative 'commend'
-require_relative 'points'
-require_relative 'register'
-require_relative 'summon'
 require_relative 'giphy'
 require_relative 'claire'
